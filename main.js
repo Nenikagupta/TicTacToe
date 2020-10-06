9d432654007f6932b22ce4fe94d6b9b5050d807e
Nenikagupta <49594853+Nenikagupta@users.noreply.github.com>
2020-10-06 23:52:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,70 @@
+// ------------costants-------------
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6], 
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+// -----------variables-----------------
+let board;
+let turn='X';
+let win;
+
+// ------------element references-----------------
+
+const squares = Array.from(document.querySelectorAll('#board div'));
+
+// ----------event listeners---------
+document.getElementById("board").addEventListener('click',handleTurn);
+
+const messages= document.querySelector('h2');
+
+document.getElementById('reset-button').addEventListener('click',init);
+
+// --------functions-----------
+
+function getWinner(){
+    let winner=null;
+
+    winningCombos.forEach((combo,index) => {
+        if(board[combo[0]] && board[combo[0]]=== board[combo[1]] && board[combo[0]] === board[combo[2]]){
+            winner=board[combo[0]];
+        }
+    });
+    return winner ? winner : board.includes('') ? null : 'T';
+}
+
+function handleTurn(e){
+    let idx= squares.findIndex(function(s){
+        return s === e.target;
+    });
+    board[idx]= board[idx] ? board[idx]: turn;
+    turn = turn === 'X'? 'O':'X';
+    win=getWinner();
+    //console.log(board);
+    render();
+};
+
+function init(){
+    board=[
+        '','','',
+        '','','',
+        '','',''
+    ];
+    render();
+};
+
+function render(){
+    board.forEach((mark,index) => {
+        squares[index].textContent = mark;
+    });
+    messages.textContent = win ==='T' ? "That's a tie!! ": win ? win+" wins the game!! ":"It's "+ turn + "'s turn !";
+};
+
+init();
+
